4349d5f0b039afd6cee7e5a99e1d65fc842396d5
jinmusic148-cpu <jinmusic148@gmail.com>
2026-08-17 02:08:48 -0500

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+// Archivo de configuración raíz del proyecto. No suele necesitar cambios.
+plugins {
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+}
